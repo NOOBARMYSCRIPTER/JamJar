e51ad12c5104819da55dcb2159461fdabaf3d07b
Okho <109078241+NOOBARMYSCRIPTER@users.noreply.github.com>
2026-05-19 21:10:00 +0500

    Update game.cpp

--- src/game.cpp
+++ src/game.cpp
@@ -183,8 +183,10 @@ private:
 
 #ifdef __EMSCRIPTEN__
             MAIN_THREAD_EM_ASM({
-                if (window.Module && typeof window.Module.updateMonsterUI === 'function') {
-                    window.Module.updateMonsterUI($0, UTF8ToString($1), $2, $3);
+                if (window.Module) {
+                    if (window.Module.updateMonsterUI) {
+                        window.Module.updateMonsterUI($0, UTF8ToString($1), $2, $3);
+                    }
                 }
             }, enemy.id, text, pctX, pctY);
 #endif
